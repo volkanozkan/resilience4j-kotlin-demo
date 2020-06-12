24a9f54f2350781eea4b282c184d8ff658e92f46
Volkan Ozkan <vozkan@bol.com>
2020-06-12 11:58:11 +0200

    package name refactor

--- src/main/kotlin/io/github/volkanozkan/resilience4jdemo/business/controller/Resource.kt
+++ src/main/kotlin/io/github/volkanozkan/resilience4jdemo/business/controller/Resource.kt
@@ -1,8 +1,8 @@
-package io.github.volkanozkan.resilience4jdemo.ratelimiter.controller
+package io.github.volkanozkan.resilience4jdemo.business.controller
 
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException
 import io.github.resilience4j.ratelimiter.RequestNotPermitted
-import io.github.volkanozkan.resilience4jdemo.ratelimiter.service.RateLimiterService
+import io.github.volkanozkan.resilience4jdemo.business.service.Service
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -12,17 +12,17 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/v1/api")
-class Resource(private val rateLimiterService: RateLimiterService) {
+class Resource(private val service: Service) {
 
     @GetMapping("/rl")
     fun callWithRateLimiter(): ResponseEntity<String> {
-        val result = rateLimiterService.callWithRateLimiter()
+        val result = service.callWithRateLimiter()
         return ResponseEntity.ok(result)
     }
 
     @GetMapping("/cb")
     fun callWithCircuitBreaker(): ResponseEntity<String> {
-        val result = rateLimiterService.callWithCircuitBreaker()
+        val result = service.callWithCircuitBreaker()
         return ResponseEntity.ok(result)
     }
 
--- src/main/kotlin/io/github/volkanozkan/resilience4jdemo/business/service/Service.kt
+++ src/main/kotlin/io/github/volkanozkan/resilience4jdemo/business/service/Service.kt
@@ -1,4 +1,4 @@
-package io.github.volkanozkan.resilience4jdemo.ratelimiter.service
+package io.github.volkanozkan.resilience4jdemo.business.service
 
 import io.github.volkanozkan.resilience4jdemo.resilience.CircuitBreakerConfiguration
 import io.github.volkanozkan.resilience4jdemo.resilience.RateLimiterConfiguration
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component
 import kotlin.random.Random
 
 @Component
-class RateLimiterService(private val resilience: Resilience) {
+class Service(private val resilience: Resilience) {
     fun callWithRateLimiter(): String {
         return resilience(
             name = "test-rl",
